b2427c9548182caa230fd0a1c55c3a97b0e71ebd
grimmjule <grimmjule262@gmail.com>
2024-10-08 17:06:19 +0200

    chore(dirichlet_neumann): multi processing

--- dirichlet_neumann.py
+++ dirichlet_neumann.py
@@ -1,6 +1,9 @@
-
+from mpi4py import MPI
 import numpy as np
 from scipy.linalg import solve
+
+comm = MPI.Comm.Clone( MPI.COMM_WORLD )
+rank = comm.Get_rank()
 
 # Size of steps (delta x) and relaxation parameter
 h = 1/3
@@ -25,8 +28,6 @@ def gamma(vec1, vec2=None):
         Gamma2 = np.array([vec1[15], vec1[19], vec1[23], vec1[27]])
     return Gamma1, Gamma2
 
-print(gamma(u1_init, u3_init))
-print(gamma(u2_init))
 
 # Matrix for rooms 1 and 3
 A1 = 1/(h**2) * np.array([[-4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -83,7 +84,7 @@ def solve_omega2(u1, u3, A):
     u2_new = solve(A, u_DC)
     return u2_new
 
-# print(solve_omega2(u1_init, u3_init, A2))
+print(solve_omega2(u1_init, u3_init, A2))
 
 u2_new = solve_omega2(u1_init, u3_init, A2)
 
@@ -114,7 +115,7 @@ def relax(u, u_new):
     u_new = omega * u_new + (1 - omega) * u
     return u_new
 
-print(relax(u1_init, u1_new))
+# print(relax(u1_init, u1_new))
 
 def reset_walls(u1, u2, u3):
     u1_walls = np.array([32.5, 15, 15, 10, 40, u1[5], u1[6], u1[7], 40, u1[9], u1[10], u1[11], 32.5, 15, 15, 15])
@@ -122,22 +123,76 @@ def reset_walls(u1, u2, u3):
     u3_walls = np.array([32.5, 15, 15, 32.5, 40, u3[5], u3[6], u3[7], 40, u3[9], u3[10], u3[11], 32.5, 15, 15, 15])
     return u1_walls, u2_walls, u3_walls
 
+#------------
 # Iteration
+#------------
+
 iterations = 10
 u1 = u1_init
 u2 = u2_init
 u3 = u3_init
 
-while iterations > 0:
-    u2_new = solve_omega2(u1, u3, A2)
-    u1_new = solve_omega1(u2_new, A1)
-    u3_new = solve_omega3(u2_new, A1)
-    u1 = relax(u1, u1_new)
-    u2 = relax(u2, u2_new)
-    u3 = relax(u3, u3_new)
-    u1, u2, u3 = reset_walls(u1, u2, u3)
-    iterations -= 1
+print('\n Initial conditions: ')
+print(f'\n u_1: \n {u1.reshape(4,4)} \n\n u_2: \n {u2.reshape(7,4)} \n\n u_3: \n {u3.reshape(4,4)}')
 
-print(u1)
-print(u2)
-print(u3)
+# while iterations > 0:
+#     u2_new = solve_omega2(u1, u3, A2)
+#     u1_new = solve_omega1(u2_new, A1)
+#     u3_new = solve_omega3(u2_new, A1)
+#     u1 = relax(u1, u1_new)
+#     u2 = relax(u2, u2_new)
+#     u3 = relax(u3, u3_new)
+#     u1, u2, u3 = reset_walls(u1, u2, u3)
+#     print(f'\n\n Iteration {11-iterations}: ')
+#     print(f'\n u_1: \n {u1.reshape(4,4)} \n\n u_2: \n {u2.reshape(7,4)} \n\n u_3: \n {u3.reshape(4,4)}')
+#     iterations -= 1
+
+
+num_iterations = 10
+
+for iteration in range(num_iterations):
+    if rank == 0:
+        # Receive u1 and u3 (old values)
+        u1 = np.empty(16)
+        u3 = np.empty(16)
+        comm.Recv(u1, source=1, tag=(iteration))
+        comm.Recv(u3, source=1, tag=(iteration+10))
+
+        # Compute u2_new
+        u2_new = solve_omega2(u1, u3, A2)
+        space1, u2_new, space3 = reset_walls(u1, u2_new, u3)
+
+        # Relaxation
+        u2_new = relax(u2, u2_new)
+
+        # Send u2_new
+        comm.Send(u2_new, dest=1, tag=iteration)
+
+        # Update u2
+        u2 = u2_new  
+
+    if rank == 1:
+        # Send u1 and u3 
+        comm.Send(u1, dest=0, tag=iteration)
+        comm.Send(u3, dest=0, tag=(iteration+10))
+
+        # Receive u2_new
+        u2_new = np.empty(28)
+        comm.Recv(u2_new, source=0, tag=iteration)
+
+        # Compute u1_new and u3_new
+        u1_new = solve_omega1(u2_new, A1)
+        u3_new = solve_omega3(u2_new, A1)
+        u1_new, space2, u3_new = reset_walls(u1_new, u2_new, u3_new)
+
+        # Relaxation
+        u1_new = relax(u1, u1_new)
+        u3_new = relax(u3, u3_new)
+
+        print(f'\n\n Iteration {iteration+1}: ')
+        print(f'\n u_1: \n {u1_new.reshape(4,4)} \n\n u_2: \n {u2_new.reshape(7,4)} \n\n u_3: \n {u3_new.reshape(4,4)}')
+
+        # Update u1 and u3
+        u1 = u1_new
+        u3 = u3_new
+
