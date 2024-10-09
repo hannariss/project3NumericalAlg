36e063cb96916d754be09ca04fc41e0e9ede4e8b
hannariss <rissbacher.h@gmail.com>
2024-10-09 12:08:27 +0200

    chore(sparse_matrix): sparse Matrix for Dirichlet condition and room 2

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -28,54 +28,99 @@ row_ind = np.array([0, 0, 0])
 col_ind = np.array([0, 1, 4]) 
 value = np.array([-4, 1, 1])
 
-
-
 n = 4
-k = np.arange(0, n**2, 1)
 
-row = []
-col = []
-value = []
+def sparse_matrix_neumann(n):
+    k = np.arange(0, n**2, 1)
 
-for k in range(len(k)):
-    i = k % n
-    j = k//n #results in an integer
-    
-    # Linker Nachbar
-    if i > 0:
+    row = []
+    col = []
+    value = []
+
+    for k in range(len(k)):
+        i = k % n
+        j = k//n #results in an integer
+
+        # lower neighbour
+        if j > 0:
+            row.append(k)
+            col.append(k-n)
+            value.append(1)
+        
+        # left neighbour
+        if i > 0:
+            row.append(k)
+            col.append(k-1)
+            value.append(1)
+
+        # diagonal
         row.append(k)
-        col.append(k-1)
-        value.append(1)
+        col.append(k)
+        # according to which k we are looking at it must be stored a -3 or -4
+        if i == (n - 1):
+            value.append(-3)
+        else:
+            value.append(-4)
 
-    #Diagonale
-    row.append(k)
-    col.append(k)
-    #je nachdem welches k wir betrachten muss eine -3 oder -4 eingetragen werden
-    if i == (n - 1):
-        value.append(-3)
-    else:
+        # right neighbour
+        if i < n - 1:
+            row.append(k)
+            col.append(k + 1)
+            value.append(1)
+
+        # upper neighbour
+        if j < n-1:
+            row.append(k)
+            col.append(k + n)
+            value.append(1)
+
+    row = np.array(row)
+    col = np.array(col)
+    value = np.array(value)
+    return csr_matrix((value, (row, col) )) * 1/(h**2)
+
+
+def sparse_matrix_dirichlet(n):
+    row = []
+    col = []
+    value = []
+
+    for k in range(n*(2*n-1)):
+        i = k % n
+        j = k//n #results in an integer
+
+        # lower neighbour
+        if j > 0:
+            row.append(k)
+            col.append(k-n)
+            value.append(1)
+        
+        # left neighbour
+        if i > 0:
+            if not (i == 1 and k < (n*(2*n-1))/2):
+                row.append(k)
+                col.append(k-1)
+                value.append(1)
+
+        # diagonal
+        row.append(k)
+        col.append(k)
         value.append(-4)
 
-    # Rechter Nachbar
-    if i < n - 1:
-        row.append(k)
-        col.append(k + 1)
-        value.append(1)
+        # right neighbour
+        if i < n - 1:
+            if not (i == n-2 and k >= (n*(2*n-1))/2):
+                row.append(k)
+                col.append(k + 1)
+                value.append(1)
 
-    # Oberer Nachbar
-    if j < n-1:
-        row.append(k)
-        col.append(k + n)
-        value.append(1)
+        # upper neighbour
+        if j < (2*n-2):
+            row.append(k)
+            col.append(k + n)
+            value.append(1)
 
-    # Unterer Nachbar
-    if j > 0:
-        row.append(k)
-        col.append(k-n)
-        value.append(1)
-
-row = np.array(row)
-col = np.array(col)
-value = np.array(value)
-
-A = csr_matrix((value, (row, col) )) * 1/(h**2)
+    row = np.array(row)
+    col = np.array(col)
+    value = np.array(value)
+    return csr_matrix((value, (row, col) )) * 1/(h**2)
