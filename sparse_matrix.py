632ce5c2eb804a02271d5f7a986b4bdc4abbf5ed
hannariss <rissbacher.h@gmail.com>
2024-10-12 13:26:05 +0200

    doc(sparse_matrix)

--- sparse_matrix.py
+++ sparse_matrix.py
@@ -3,43 +3,35 @@ import numpy as np
 
 h=1/3
 
-A1 = 1/(h**2) * np.array([[-4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
-                          [1, -4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 1, -4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 1, -3, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-                          [1, 0, 0, 0, -4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-                          [0, 1, 0, 0, 1, -4, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-                          [0, 0, 1, 0, 0, 1, -4, 1, 0, 0, 1, 0, 0, 0, 0, 0],
-                          [0, 0, 0, 1, 0, 0, 1, -3, 0, 0, 0, 1, 0, 0, 0, 0],
-                          [0, 0, 0, 0, 1, 0, 0, 0, -4, 1, 0, 0, 1, 0, 0, 0],
-                          [0, 0, 0, 0, 0, 1, 0, 0, 1, -4, 1, 0, 0, 1, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 1, 0, 0, 1, -4, 1, 0, 0, 1, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, -3, 0, 0, 0, 1],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, -4, 1, 0, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, -4, 1, 0],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, -4, 1],
-                          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, -3]])
+#Add on 3a:
+#for room 1, 3 geom_room is n**2, for room 4 geom_room would be 2*(n/2)
 
-A = csr_matrix(A1)
+n = 4 # CAUTION: when considering room 4, n needs to be an odd number!
+h = 1/3
+def sparse_matrix_smallroom(n, h, geom_room):
+    """
+    creates a sparse matrix for room 1, 3, 4
 
-# Sparse matrix: row_ind, col_ind, value
-
-row_ind = np.array([0, 0, 0])
-col_ind = np.array([0, 1, 4]) 
-value = np.array([-4, 1, 1])
-
-n = 4
-
-def sparse_matrix_smallroom(n):
-    k = np.arange(0, n**2, 1)
-
+    Paramaters:
+    n: int
+        number of 'points' on wall with given mesh width
+    h: float
+        mesh width
+    geom_room: int
+        defines the geometry of the room, for room 1, 3 it's n**2 for room 4 it's 2*(n/2)
+    
+    Returns:
+    out: scipy sparse csr_matrix
+        Sparse matrix for the room 1, 3 or 4
+    """
+    # initiate lists for sparse matrix command
     row = []
     col = []
     value = []
 
-    for k in range(len(k)):
+    for k in range(geom_room):
         i = k % n
-        j = k//n #results in an integer
+        j = k//n # results in an integer
 
         # lower neighbour
         if j > 0:
@@ -77,13 +69,28 @@ def sparse_matrix_smallroom(n):
                 col.append(k + n)
                 value.append(1)
 
+    # convert lists to arrays (csr_matrix command needs np.arrays as input)
     row = np.array(row)
     col = np.array(col)
     value = np.array(value)
     return csr_matrix((value, (row, col) )) * 1/(h**2)
 
 
-def sparse_matrix_bigroom(n):
+def sparse_matrix_bigroom(n, h):
+    """
+    creates a sparse matrix for room 2
+
+    Paramaters:
+    n: int
+        number of 'points' on wall with given mesh width
+    h: float
+        mesh width
+    
+    Returns:
+    out: scipy sparse csr_matrix
+        Sparse matrix for the room 2
+    """
+    # initiate lists for sparse matrix command
     row = []
     col = []
     value = []
@@ -125,6 +132,7 @@ def sparse_matrix_bigroom(n):
                 col.append(k + n)
                 value.append(1)
 
+    # convert lists to arrays (csr_matrix command needs np.arrays as input)
     row = np.array(row)
     col = np.array(col)
     value = np.array(value)
