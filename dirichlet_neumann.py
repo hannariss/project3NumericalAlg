fb004884c19a53eb48ac7b58a3076c7b95b5dd62
hannariss <rissbacher.h@gmail.com>
2024-10-14 10:38:01 +0200

    cleanup(dirichlet_neumann)

--- dirichlet_neumann.py
+++ dirichlet_neumann.py
@@ -6,21 +6,18 @@ h = 1/100 # Size of steps (delta x)
 
 A1 = sparse_matrix_smallroom(h, room_4=False) # Coefficient matrix A for rooms 1 and 3
 A2 = sparse_matrix_bigroom(h) # Coefficient matrix A for room 2
-A3 = sparse_matrix_smallroom(h)
 
 # Create flat object
 #apartment = dn.DN_Method(h, t_n=15, t_h=40)
-apartment2 = dn.DN_Method4Rooms(h)
 
 # # Dirichlet Neumann Iteration40
 #apartment.dn_iteration(A1, A2)
-apartment2.dn_iteration(A1, A2, A3)
-## Project 3a:
 
-# A3 = sparse_matrix_smallroom(4, h, 16) Coefficient matrix A for room 4
+## Project 3a:
+A3 = sparse_matrix_smallroom(h) # Coefficient matrix A for room 4
 
 # # Create flat object
-# apartment = dn.DN_Method4Rooms(h)
+#apartment = dn.DN_Method4Rooms(h)
 
-# # Dirichlet Neumann Iteration
+# Dirichlet Neumann Iteration
 # apartment.dn_iteration(A1, A2, A3)
